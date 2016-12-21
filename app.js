4e379d8aee3842541da36b15b7ec2acab3cbf33b
Rafaela <rafaela.vm@gmail.com>
2016-12-21 20:41:22 -0200

    1. Instalacao do nodejs, Express, EJS e Nodemon. (7)

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ var express = require('express');
 
 var app = express();
 
+//set(): definir variáveis pra dentro do express
+app.set('view engine', 'ejs');
+
 //configuração de rota
 app.get('/produtos',function(request,response){
     
+    console.log('listando...');
+
     //send(): método que envia um retorno direto pro cliente
-    response.send("<html><body><h1>Listagem de produtos</h1></body></html>");
+    //response.send("<html><body><h1>Listagem de produtos</h1></body></html>");
+
+    //chamando pela pasta
+    response.render('produtos/lista');
+
 });
 
 app.listen(4000,function(){
