eeed73f919c6546c4175e0a71062d4f9d4eb74cf
Rafaela <rafaela.vm@gmail.com>
2016-12-27 00:59:05 -0200

    2.Coesão e Acoplamento, organizando o código (3) - instalacao do pacote mysql do npm e configuracao

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,9 +1,23 @@
 //configuração de rota
 
 module.exports = function (app) {
-    app.get('/produtos', function (request, response) {
+    app.get('/produtos', function (req, res) {
+
+        //conexão com o mysql
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            port: '3306',
+            database: 'lojasistemapagamento'
+        });
+        connection.query('SELECT *FROM livros', function(err, results){
+            res.send(results);
+        });
+        connection.end();
 
         //chamando pela pasta
-        response.render('produtos/lista');
+        //res.render('produtos/lista');
     });
 }
