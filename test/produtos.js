a7809dd8bd02945c59e68d68eeb573431cd580aa
Rafaela <rafaela.vm@gmail.com>
2017-02-10 15:14:01 -0200

    07 (1) Testes de integração

---
+++ test/produtos.js
@@ -0,0 +1,7 @@
+describe('ProdutosController', function(){
+
+    //verificar se a nossa lisatagem se produz json está funcionando
+    it('#listagemJson', function(){
+        console.log('teste de verificação de listagem de json');
+    });
+});
