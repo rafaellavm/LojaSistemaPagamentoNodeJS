4eb7c8ec273733d1ff339eb00a48e55acc1ad18d
Rafaela <rafaela.vm@gmail.com>
2017-02-14 15:33:35 -0200

    07 (2) Testes de integração. Criando método de teste para listagem dos produtos usando o Mocha

--- test/produtos.js
+++ test/produtos.js
@@ -1,7 +1,26 @@
-describe('ProdutosController', function(){
+var http = require('http');
+describe('ProdutosController', function () {
 
     //verificar se a nossa lisatagem se produz json está funcionando
-    it('#listagemJson', function(){
-        console.log('teste de verificação de listagem de json');
+    it('listagem json', function (funcaoFinalizacao) {
+        var configuracoes = {
+            hostname: 'localhost',
+            port: 4000,
+            path: '/produtos',
+            headers: {
+                'Accept': 'application/json'  
+            }
+        };
+
+        http.get(configuracoes, function (res) {
+            if(res.statusCode == 200){
+                console.log('status está ok!');
+            }
+            if(res.headers['content-type'] == 'application/json; charset=utf-8'){
+                console.log('Content-type está ok!');
+            } 
+
+            funcaoFinalizacao();
+        });
     });
 });
